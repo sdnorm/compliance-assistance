6deaee9fe60c93d63c82f291df9a54db22b8a45e
Spencer Norman <spencerdnorman@gmail.com>
2020-12-11 11:13:52 -0600

    added location scaffold and did some more app setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
+import "../stylesheets/application"
+
+import { Tooltip, Toast, Popover } from 'bootstrap'
---
+++ db/migrate/20201211170258_create_locations.rb
@@ -0,0 +1,14 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
