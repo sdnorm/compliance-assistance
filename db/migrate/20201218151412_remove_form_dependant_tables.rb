c0983686d640231f8541f7bd9f6f6547fa949ac6
Spencer Norman <spencerdnorman@gmail.com>
2021-01-13 10:36:23 -0600

    removed bad migration files

--- db/migrate/20201218151412_remove_form_dependant_tables.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFormDependantTables < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :form_questions, :forms
-    remove_foreign_key :forms, :locations
-  end
-end
--- db/migrate/20201218152041_drop_form_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropFormTables < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :form_questions, :questions
-    drop_table :form_questions 
-    drop_table :forms
-  end
-end
